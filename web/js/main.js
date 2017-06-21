e31a49ddcb8b6bf9d56c9d0a80cd5cfc2b4f5a86
Gauthier <gcornette@elixis.com>
2017-06-21 16:24:49 +0200

    Update web/index.html and web/js/main.js - get books with google api

---
+++ web/js/main.js
@@ -0,0 +1,35 @@
+function bookSearch(){
+    // console.log('this function runs !');
+    var search = $("#search").val();
+    $("#results").append("");
+    console.log(search);
+
+    $.ajax({
+        url: "https://www.googleapis.com/books/v1/volumes?q=" + search,
+        dataType: "json",
+        type: "GET",
+
+        success: function(data){
+            //console.log(data);
+            for(i = 0; i < data.items.length; i++){
+                $('#results').append("<div class='panel panel-default'>"
+                                        +"<div class='panel-heading'>"
+                                            + "<span class='text-left'><strong>" + data.items[i].volumeInfo.title + "</strong></span>"
+                                            + "<span class='pull-right'><em>" + data.items[i].volumeInfo.authors + "</em></span>"
+                                        + "</div>"
+                                        + "<div class='panel-body'>"
+                                            + "<img src='"+ data.items[i].volumeInfo.imageLinks.smallThumbnail + "' class='img-responsive img-rounded'> <br> <hr>"
+                                            + "<p>" + data.items[i].volumeInfo.description + "</p>"
+                                        + "</div>"
+                                        + "<div class='panel-footer'>"
+                                            + "<span class='text-left'>" + data.items[i].volumeInfo.publisher + "</span>"
+                                            + "<span class='pull-right'>" + data.items[i].volumeInfo.publishedDate + "</span>"
+                                        + "</div>"
+                                    +"</div>");
+            }
+        },
+
+    });
+}
+
+$("#button").click(function(){bookSearch()});
