fa61c61d5822de974670b6e0372f08870faea7ac
Unknown <yassir.chakiri12@gmail.com>
2017-07-11 19:59:28 +0200

    [Test][Admin] get form Design

--- src/AppBundle/Controller/AdminController.php
+++ src/AppBundle/Controller/AdminController.php
@@ -2,15 +2,34 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\Design;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 
 class AdminController extends Controller
 {
     /** Page index admin**/
+
     public function indexAction(Request $request)
     {
-        return $this->render('club/admin/index.html.twig');
+        $design = new  Design();
+
+        $em = $this->getDoctrine()->getManager();
+        $design = $em->getRepository('AppBundle:Design')->findAll();
+
+        $editForm = $this->createForm('AppBundle\Form\DesignType', $design);
+        $editForm->handleRequest($request);
+
+        if ($editForm->isSubmitted() && $editForm->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('admin');
+        }
+
+        return $this->render('club/admin/index.html.twig', array(
+            'design' => $design,
+            'edit_form' => $editForm->createView(),
+        ));
     }
 
 }
