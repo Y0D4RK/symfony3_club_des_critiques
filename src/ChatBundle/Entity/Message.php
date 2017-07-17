e99515aa54001e7e0859a2a37d51218684bfd971
Gauthier <mysterandco@gmail.com>
2017-07-17 19:41:08 +0200

    Add message entity

---
+++ src/ChatBundle/Entity/Message.php
@@ -0,0 +1,128 @@
+<?php
+
+namespace ChatBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Message
+ *
+ * @ORM\Table(name="message")
+ * @ORM\Entity(repositoryClass="ChatBundle\Repository\MessageRepository")
+ */
+class Message
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="message", type="string", length=255)
+     */
+    private $message;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="users", type="string", length=255)
+     */
+    private $users;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="room", type="string", length=255)
+     */
+    private $room;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set message
+     *
+     * @param string $message
+     *
+     * @return Message
+     */
+    public function setMessage($message)
+    {
+        $this->message = $message;
+
+        return $this;
+    }
+
+    /**
+     * Get message
+     *
+     * @return string
+     */
+    public function getMessage()
+    {
+        return $this->message;
+    }
+
+    /**
+     * Set users
+     *
+     * @param string $users
+     *
+     * @return Message
+     */
+    public function setUsers($users)
+    {
+        $this->users = $users;
+
+        return $this;
+    }
+
+    /**
+     * Get users
+     *
+     * @return string
+     */
+    public function getUsers()
+    {
+        return $this->users;
+    }
+
+    /**
+     * Set room
+     *
+     * @param string $room
+     *
+     * @return Message
+     */
+    public function setRoom($room)
+    {
+        $this->room = $room;
+
+        return $this;
+    }
+
+    /**
+     * Get room
+     *
+     * @return string
+     */
+    public function getRoom()
+    {
+        return $this->room;
+    }
+}
+
