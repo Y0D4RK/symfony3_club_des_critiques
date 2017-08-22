343b79fd94d824bebf56e2d17dfe3810ea31c1cf
Gauthier <mysterandco@gmail.com>
2017-08-22 16:52:11 +0200

    Update chat

--- socketio/app.js
+++ socketio/app.js
@@ -48,59 +48,23 @@ io.on('connection', function(socket) {
         socket.emit('newuser', users[u]);
     }
 
-    socket.on('login', function(user) {
-
-        db.query('SELECT * FROM user WHERE id = ?', [user.id], function (err, rows, fields) {
-            if (err) {
-                console.log(err.code);
-                return false;
-            }
-            if (rows.length === 1) {
-                me = {
-                    username: rows[0].username,
-                    id: rows[0].id,
-                    avatar: 'https://gravatar.com/avatar/' + md5(rows[0].email) + '?s=100',
-                };
-                socket.emit('logged');
-
-                users[me.id] = me;
-                io.sockets.emit('newuser', me);
-
-                getLastMessages();
-            } else {
-                io.sockets.emit('error', 'Aucun utilisateur trouvé !');
-            }
-        });
+    socket.on('login', function(user, room) {
+        getUser(user);
+        getLastMessagesRooms(room);
     });
 
     /* On recoit un nouveau msg */
     socket.on('newmsg', function(message){
-
         if(message.message === ''){
             return false;
         }
-
         console.log(message);
 
         message.user = me;
         message.created_at = Date.now();
         message.room_id = 1;
 
-        var query = db.query('INSERT INTO message SET user_id=?, room_id=?, message=?, created_at=?', [
-            message.user.id,
-            message.room_id,
-            message.message,
-            new Date(message.created_at)
-        ], function(err){
-            if(err){
-                console.log(err);
-            }
-            io.sockets.emit('newmsg', message);
-        });
-
-        console.log(query.sql)
-
-
+        saveMessage(message);
     });
 
 
@@ -112,12 +76,88 @@ io.on('connection', function(socket) {
         io.sockets.emit('disuser', me);
     });
 
-    var getLastMessages = function(){
-        var queryLastMsg = db.query(''+
-            'SELECT user.id as user_id, user.username, user.email, message.message, UNIX_TIMESTAMP(message.created_at) as created_at '+
+    var saveMessage = function(message){
+        var query = db.query('INSERT INTO message SET user_id=?, room_id=?, message=?, created_at=?', [
+            message.user.id,
+            message.room_id,
+            message.message,
+            new Date(message.created_at)
+        ], function(err){
+            if(err){
+                console.log(err);
+            }
+            io.sockets.emit('newmsg', message);
+        });
+        console.log(query.sql)
+    };
+
+    var getUser = function(user){
+        var queryUser = db.query('SELECT * FROM user WHERE id = ?',
+
+            [user.user_id],
+
+            function (err, rows, fields) {
+                if (err) {
+                    console.log(err.code);
+                    return false;
+                }
+
+                if (rows.length === 1) {
+                    me = {
+                        username: rows[0].username,
+                        id: rows[0].id,
+                        avatar: 'https://gravatar.com/avatar/' + md5(rows[0].email) + '?s=100',
+                    };
+                    socket.emit('logged');
+
+                    users[me.id] = me;
+                    io.sockets.emit('newuser', me);
+
+                    getLastMessagesRooms(1);
+                } else {
+                    io.sockets.emit('error', 'Aucun utilisateur trouvé !');
+                }
+            });
+        // console.log(queryUser.sql);
+    };
+
+    var getRoom = function(room){
+        var user = db.query('SELECT * FROM room WHERE id = ?',
+
+            [room.user_id],
+
+            function (err, rows, fields) {
+                if (err) {
+                    console.log(err.code);
+                    return false;
+                }
+
+                if (rows.length === 1) {
+                    me = {
+                        username: rows[0].username,
+                        id: rows[0].id,
+                        avatar: 'https://gravatar.com/avatar/' + md5(rows[0].email) + '?s=100',
+                    };
+                    socket.emit('logged');
+
+                    users[me.id] = me;
+                    io.sockets.emit('newuser', me);
+
+                    getLastMessagesRooms(1);
+                } else {
+                    io.sockets.emit('error', 'Aucun utilisateur trouvé !');
+                }
+            });
+        // console.log(user.sql);
+    }
+
+    var getLastMessagesRooms = function(roomId){
+        var queryLastMsgRooms = db.query(''+
+            'SELECT message.message, user.username, user.email, UNIX_TIMESTAMP(message.created_at) as created_at '+
             'FROM message '+
-            'INNER JOIN user ON user.id = message.user_id '+
-            'LIMIT 10', function(err, rows){
+            'LEFT JOIN user ON user.id = message.user_id '+
+            'WHERE message.room_id = '+roomId+
+            ' GROUP BY message.id', function(err, rows){
             if(err){
                 socket.emit('error', err.code);
                 return true;
@@ -139,9 +179,8 @@ io.on('connection', function(socket) {
                 socket.emit('newmsg', message);
             }
         });
-        console.log(queryLastMsg.sql);
+        console.log(queryLastMsgRooms.sql);
     };
-
 });
 
 server.listen(4200);
